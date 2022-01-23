eb85849a9a7ba8ed4e57cf53497337eaf9bbe790
andru196 <andru196@icloud.com>
2022-01-23 20:14:28 +0300

    state + toggle

--- swiftuiTest/ContentView.swift
+++ swiftuiTest/ContentView.swift
@@ -8,22 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-//    var body: some View {
-//        Text("Hello World")
-//            .font(.largeTitle)
-//            .padding()
-//            .foregroundColor(.white)
-//            .background(LinearGradient(colors: [.black, .white], startPoint: .top, endPoint: .bottom))
-//    }
-    
+    @State private var showHello = true
     var body: some View {
-        let colors = Gradient(colors: [.red, .green, .yellow, .blue, .purple])
-//        let gradient = RadialGradient(gradient: colors, center: .center, startRadius: 50, endRadius: 200)
-        let gradient = AngularGradient(gradient: colors, center: .center)
-        return Circle()
-            .strokeBorder(gradient, lineWidth: 20)
-//            .fill(gradient)
-//            .frame(width: 400, height: 400)
+        //property wrapper: @State
+        VStack {
+            Toggle(isOn: $showHello){
+                Text("Show Hello")
+            }
+            if showHello {
+                Text("Hello, World!")
+            }
+        }
+        
     }
     
     

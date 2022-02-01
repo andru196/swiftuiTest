6f6366c1d90ada71157a172fb06295fea9132153
andru196 <andru196@icloud.com>
2022-02-01 22:43:21 +0300

    TabView and Group

--- swiftuiTest/ContentView.swift
+++ swiftuiTest/ContentView.swift
@@ -1,44 +1,45 @@
 import SwiftUI
 
 //struct ContentView: View {
-//    var body: some View {
-//        NavigationView {
-//            Text("SwiftUI")
-//                .navigationTitle("WellCum")
-//                .navigationBarTitleDisplayMode(.inline)
-//                .navigationBarItems(trailing: HStack {
-//                    Button("Help") {
-//                        print("Help tapped")
-//                    }
 //
-//                    Button("About") {
-//                        print("About tapped")
-//                    }
-//                })
+//    @State private var selectedView = 1
+//
+//    var body: some View {
+//        TabView(selection: $selectedView) {
+//            Text("First View")
+//                .tabItem {
+//                    Image(systemName: "1.circle")
+//                    Text("First")
+//                } .tag(1)
+//            Text("Second View")
+//                .tabItem {
+//                    Image(systemName: "2.circle")
+//                    Text("Second")
+//                } .tag(2)
 //        }
 //    }
 //}
-//
 
 struct ContentView: View {
-    @State private var users = ["Users", "Maria", "Igor"]
+        
     var body: some View {
-        NavigationView {
-            List {
-                ForEach (users, id:\.self) {
-                    Text($0)
-                } .onDelete(perform: delete)
-                    .onMove(perform: move)
+        VStack {
+            Group {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
             }
-            .navigationBarItems(trailing: EditButton())
+            
+            Group {
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+                Text("Hello, World!")
+            }
         }
-    }
-    func delete (at offsets: IndexSet) {
-        users.remove(atOffsets: offsets)
-    }
-    
-    func move (from source: IndexSet, to destination: Int) {
-        users.move(fromOffsets: source, toOffset: destination)
     }
 }
 

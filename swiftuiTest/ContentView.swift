6a496ffa4fead5a9d807043d10c7a91e596548df
andru196 <andru196@icloud.com>
2022-02-01 23:31:13 +0300

    Alert and ActionSheet

--- swiftuiTest/ContentView.swift
+++ swiftuiTest/ContentView.swift
@@ -1,49 +1,65 @@
 import SwiftUI
 
+//struct ContentView: View {
+//
+//    @State private var showingALert1 = false
+//    @State private var showingALert2 = false
+//
+//    var body: some View {
+//        HStack {
+//            Button("Show 1") {
+//                self.showingALert1 = true
+//            } .alert(isPresented: $showingALert1) {
+//                Alert(title: Text("One"),
+//                      message: Text("Some detail msg"),
+//                      dismissButton: .default(Text("OK")))
+//            }
+//
+//            Button("Show 2") {
+//                self.showingALert2 = true
+//            } .alert(isPresented: $showingALert2) {
+//                Alert(title: Text("Two"),
+//                      message: Text("Some detail msg"),
+//                      dismissButton: .cancel())
+//            }
+//        }
+//    }
+//}
+
+//struct ContentView: View {
+//
+//    @State private var showingALert = false
+//
+//    var body: some View {
+//        Button(action: {
+//            self.showingALert = true
+//        }) {
+//            Text("Show Alert")
+//        } .alert(isPresented: $showingALert) {
+//            Alert(title: Text("Are u sure u esmy to delete this?"), message: Text("There is no wat back"), primaryButton: .destructive(Text("Delete")) {
+//                print("Deleting...")
+//            }, secondaryButton: .cancel())
+//
+//
+//        }
+//    }
+//}
+
 struct ContentView: View {
     
-    @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
-    @State private var tipPercentage = 0
-    
-    let tipPercentages = [0, 5, 10, 15, 20, 25, 30, 50]
-
-    var totalPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
-        let tipSelection = Double(tipPercentages[tipPercentage])
-        let orderAmount = Double(checkAmount) ?? 0
-        
-        let tipValue = orderAmount / 100 * tipSelection
-        let grandTotal = orderAmount + tipValue
-        let amountPerPerson = grandTotal / peopleCount
-        return amountPerPerson
-    }
+    @State private var showingALert = false
     
     var body: some View {
-        NavigationView {
-            Form {
-                Section {
-                    TextField("Amount", text: $checkAmount)
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach (2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
-                }
-                Section (header: Text("Сколько чаевых вы хотите оставить")) {
-                    Picker ("Tip percentage", selection: $tipPercentage) {
-                        ForEach(0..<tipPercentages.count) {
-                            Text("\(self.tipPercentages[$0])%")
-                                
-                        }
-                    }.pickerStyle(SegmentedPickerStyle())
-                }
-                Section {
-                    Text("\(totalPerson, specifier: "%.2f")")
-                }
-            }
-            .navigationBarTitle("Калькулятор чаевых", displayMode: .inline)
+        Button(action: {
+            self.showingALert = true
+        }) {
+            Text("Show action Sheets")
+        } .actionSheet(isPresented: $showingALert) {
+            ActionSheet(title: Text("What do you want to do?"),
+                        message: Text("There is only one choise"),
+                        buttons: [.default(Text("Dismiss Action Sheet")),
+                                  .cancel(),
+                                  .destructive(Text("Delete"))])
         }
     }
 }
-

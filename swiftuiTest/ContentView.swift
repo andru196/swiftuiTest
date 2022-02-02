f32a5b4607eb97e1dc85650644697f0e10b7f096
andru196 <andru196@icloud.com>
2022-02-02 21:48:43 +0300

    NavigationLink and sheet

--- swiftuiTest/ContentView.swift
+++ swiftuiTest/ContentView.swift
@@ -1,46 +1,94 @@
 import SwiftUI
 
-// property wrappers:
-// @State - подходит для структур
-// @ObservedObject - требует экземпляры ObservableObject
-// @EnvironmentObject - можно делиться даннами со всеми view 
-// @Published
+/// First part
+//struct DetailView: View {
+//    var body: some View {
+//        Text("This is the detail view")
+//    }
+//}
+//
+//
+//struct ContentView: View {
+//
+//    var body: some View {
+//        NavigationView {
+//            VStack {
+//                NavigationLink(destination: DetailView()) { Text("Show detail View")
+//                }.navigationBarTitle("Navigation")
+//            }
+//        }
+//    }
+//}
 
-
-//class User: ObservableObject {
-//    @Published var firstName = "Ivan"
-//    @Published var lastName = "Petrov"
+/// Second part
+//struct Dog: Identifiable {
+//    var id = UUID()
+//    var name: String
+//}
+//
+//struct DogRow: View {
+//    var dog: Dog
+//
+//    var body: some View {
+//        Text(dog.name)
+//    }
+//}
+//
+//struct DogView: View {
+//    var dog: Dog
+//
+//    var body: some View {
+//        Text("Come and choose a \(dog.name)")
+//    }
 //}
 //
 //struct ContentView: View {
 //
-//    @ObservedObject var user = User()
-//
 //    var body: some View {
-//        VStack() {
-//            Text("Your name is \(user.firstName) \(user.lastName)")
-//            TextField("First Name", text: $user.firstName)
-//            TextField("Last Name", text: $user.lastName)
+//        let first = Dog(name: "Sobaka")
+//        let second = Dog(name: "Doggg")
+//        let dogs = [first, second]
+//
+//        return NavigationView {
+//
+//            List(dogs) { dog in
+//                NavigationLink(destination: DogView(dog: dog)) {
+//                    DogRow(dog: dog)
+//                }
+//            } .navigationTitle("Choose a dog")
 //        }
 //    }
 //}
 
 
-struct ContentView: View {
+/// Third part
+
+struct DetailView: View {
     
-    @ObservedObject var settings = UserSettings()
-    
+    @Environment(\.presentationMode) var presentationMode
     var body: some View {
-        VStack {
-            Text("Your settings is \(settings.score)")
-            Button(action: {
-                self.settings.score += 1
-            }) {
-                Text("Increase Score")
-            }
+        Text("Detail")
+        Button("Back") {
+            self.presentationMode.wrappedValue.dismiss()
         }
     }
 }
+
+struct ContentView: View {
+
+    @State private var showingDetail = false
+    
+    var body: some View {
+        Button(action: {
+            self.showingDetail.toggle()
+        }) {
+            Text("Show detail")
+        } .sheet(isPresented: $showingDetail) {
+            DetailView()
+        }
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
